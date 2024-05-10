48856558b10c4132f3ab9c550e703ca5e23a9eb5
alirezavafakhah <avf69programmer@gmail.com>
2024-05-10 03:43:34 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'aebb8d0c-3e1a-40c3-8c6f-1bc0ca1c5881';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
